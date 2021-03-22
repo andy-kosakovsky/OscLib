a54ff7d774b622c98a7c90c5e92e9516e7ed1368
Andy <aklt92@gmail.com>
2021-03-22 09:43:41 +0000

    timestamp/timetag termitology changes

--- OscLib/Source/Items/OscTimetag.cs
+++ OscLib/Source/Items/OscTimetag.cs
@@ -5,32 +5,31 @@ using System.Text;
 namespace OscLib
 {
     /// <summary>
-    /// Describes an OSC Timestamp both as a 64 bit fixed-point NTP timestamp and ticks used by DateTime and TimeSpan.
+    /// Describes an OSC Timetag both as a 64 bit fixed-point NTP timestamp and a tick value useable by DateTime and TimeSpan.
     /// </summary>
-    public readonly struct OscTimestamp
+    public readonly struct OscTimetag
     {       
         private readonly ulong _ntpTimestamp;
         private readonly long _ticks;
 
-        /// <summary> This timestamp represented in NTP format. </summary>
+        /// <summary> This timetag represented in NTP timestamp format. </summary>
         public ulong NtpTimestamp { get => _ntpTimestamp; }
 
-        /// <summary> This timestamp represented as DateTime ticks, UTC. </summary>
+        /// <summary> This timetag represented as DateTime ticks, UTC. </summary>
         public long Ticks { get => _ticks; }
 
-        /// <summary> Returns a DateTime representation of this timestamp, corresponding to the local time of the system. Creates a new DateTime, so careful with that GC, Eugene. </summary>
+        /// <summary> Returns this timetag represented as a DateTime object, corresponding to the time zone of the system. Creates a new instance of a DateTime object. </summary>
         public DateTime DateTime { get => TimeZoneInfo.ConvertTimeFromUtc(new DateTime(_ticks), TimeZoneInfo.Local); }
 
-        /// <summary> Returns a DateTime representation of this timestamp, as UTC time. Creates a new DateTime, ditto about being careful with that GC. </summary>
+        /// <summary> Returns this timetag represented as a DateTime object, corresponding to UTC time. Creates a new instance of a DateTime object. </summary>
         public DateTime DateTimeUtc { get => new DateTime(_ticks); }
             
         /// <summary>
-        /// Creates an OSC Timestamp out of the provided DateTime object.
+        /// Creates an OSC Timetag out of the provided DateTime object.
         /// </summary>
-        /// <param name="time"> The DateTime object to create an OSC Timestamp out of. </param>
-        public OscTimestamp(DateTime time)
+        /// <param name="time"> The source DateTime object. </param>
+        public OscTimetag(DateTime time)
         {
-
             _ticks = time.ToUniversalTime().Ticks;
        
             long tickMinusEpoch = _ticks - OscTime.NtpEpochStart;
@@ -43,14 +42,13 @@ namespace OscLib
             uint fraction = (uint)(uint.MaxValue * ((double)tickFraction / TimeSpan.TicksPerSecond));
 
             _ntpTimestamp = (ulong)seconds << 32 | fraction;
-
         }
 
         /// <summary>
-        /// Creates an OSC Timestamp using DateTime or TimeSpan ticks. Ticks need to adhere to UTC in order to produce right results.
+        /// Creates an OSC Timetag using DateTime or TimeSpan ticks. Ticks need to adhere to UTC time in order to produce correct results.
         /// </summary>
         /// <param name="tick"> UTC-based tick value. </param>
-        public OscTimestamp(long tick)
+        public OscTimetag(long tick)
         {
 
             _ticks = tick;
@@ -71,10 +69,10 @@ namespace OscLib
         }
 
         /// <summary>
-        /// Creates an OSC timestamp out of an NTP-format timestamp. 
+        /// Creates an OSC Timetag out of an NTP-format timestamp. 
         /// </summary>
         /// <param name="ntpTimestamp"> An ulong containing a 64-bit fixed-point NTP-format timestamp. </param>
-        public OscTimestamp(ulong ntpTimestamp)
+        public OscTimetag(ulong ntpTimestamp)
         {
             _ntpTimestamp = ntpTimestamp;
             _ticks = 0;
@@ -89,7 +87,7 @@ namespace OscLib
         }
 
         /// <summary>
-        /// Returns timestamp as a string, formatted as "day/month/year hours:minutes:seconds:milliseconds".
+        /// Returns this timetag as a string, formatted as "day/month/year hours:minutes:seconds:milliseconds".
         /// </summary>
         /// <returns></returns>
         public override string ToString()
