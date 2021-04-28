ddfc88a40aef3f7866fb711b0c7d388b34dd8ec6
Andy <aklt92@gmail.com>
2021-04-28 21:08:38 +0100

    whoops

--- OscLib/Source/Workers/OscLink.cs
+++ OscLib/Source/Workers/OscLink.cs
@@ -286,6 +286,8 @@ namespace OscLib
         /// <exception cref="SocketException"> Couldn't open a socket. </exception>
         public void OpenToTarget(int port, IPEndPoint targetEndPoint)
         {
+            // testing
+
             if (targetEndPoint == null)
             {
                 throw new ArgumentNullException(nameof(targetEndPoint));
